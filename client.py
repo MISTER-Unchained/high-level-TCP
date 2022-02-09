ee1c1ab915c46161f7f5b07120ad2a0d60f226ae
MASTER-Unchained <djangofakkeldij@gmail.com>
2022-02-09 22:34:15 +0100

    Server pretty much perfect

--- client.py
+++ client.py
@@ -1,24 +1,61 @@
 import socket
+import threading as th
 
-HOST = "0.0.0.0"
+HOST = "localhost"
 PORT = 5000
 
-end = "EOM".encode("utf-8")
 
+datatosend = bytes()
 
+global_socket = None
+
+end = "$EOM".encode("utf-8")
+close = "$CLOSE_ENTIRE_CONNECTION".encode("utf-8")
 def check_valid_message(mes: bytes):
     if mes.find(end) == -1:
         return True
     else:
         return False
 
+def check_end(data: bytes):
+    ind = data.find(end)
+    if ind == -1:
+        return False, data
+    else:
+        return data[0:ind], data[ind+len(end):]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+def handle_data(output):
+    print(f"server returned with \"{output}\"")
+
+def connection_handler():
+    global datatosend
+    global global_socket
+    data = bytes()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        global_socket = s
+        s.connect((HOST, PORT))
+        while True:
+            data += s.recv(1024)
+            output, data = check_end(data)
+            if output:
+                handle_data(output.decode("utf-8"))
+            if data.find(close) != -1:
+                s.sendall(close)
+                print("connection closed")
+                break
+    print("exiting")
+
+th1 = th.Thread(target=connection_handler, daemon=True)
+th1.start()
+
+def get_input_loop():
+    global datatosend
+    global global_socket
     while True:
-        mes = (input("enter message:")).encode("utf-8")
-        if check_valid_message(mes):
-            s.sendall(mes + end)
+        datatosend = input("enter command:").encode("utf-8")
+        if datatosend == close:
+            global_socket.sendall(close)
+            break
         else:
-            print("invalid message")
-            continue
+            global_socket.sendall(datatosend + end)
+get_input_loop()
--- server.py
+++ server.py
@@ -1,43 +1,52 @@
 import socket
 
-HOST = "0.0.0.0"
+HOST = "localhost"
 PORT = 5000
 
 
 def handle_data(output):
-    print(output)
+    return (f"ok + {output}").encode("utf-8")
+
+def main(out_fun):
+    end = "$EOM".encode("utf-8")
+    close = "$CLOSE_ENTIRE_CONNECTION".encode("utf-8")
+    def check_end(data: bytes):
+        ind = data.find(end)
+        if ind == -1:
+            return False, data
+        else:
+            return data[0:ind], data[ind+len(end):]
+
+    def check_valid_message(mes: bytes):
+        if mes.find(end) == -1:
+            return True
+        else:
+            return False
 
 
-end = "EOM".encode("utf-8")
-close = "CLOSE_ENTIRE_CONNECTION".encode("utf-8")
-def check_end(data: bytes):
-    ind = data.find(end)
-    if ind == -1:
-        return False, data
-    else:
-        return data[0:ind], data[ind+len(end):]
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by: @{addr}")
+            data = bytes()
+            while True:
+                data += conn.recv(1024)
+                output, data = check_end(data)
+                if output:
+                    return_data = out_fun(output)
+                    if check_valid_message(return_data):
+                        if return_data.find(close) == -1:
+                            conn.sendall(return_data + end)
+                        else:
+                            conn.sendall(return_data + end + close)
+                    else:
+                        raise("Contained invalid data")
+                if data.find(close) != -1:
+                    conn.sendall(close)
+                    print("connection closed")
+                    break
+    print("exiting")
 
-def check_valid_message(mes: bytes):
-    if mes.find(end) == -1:
-        return True
-    else:
-        return False
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by: @{addr}")
-        data = bytes()
-        while True:
-            data += conn.recv(1024)
-            output, data = check_end(data)
-            if output:
-                handle_data(output.decode("utf-8"))
-            if data.find(close) != -1:
-                print("connection closed")
-                break
-
-            
+main(handle_data)
